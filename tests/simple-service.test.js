adc081040ed99e3173a13fc64d8582a52a15e38e
Adele Bendayan <adele.bendayan@gmail.com>
2022-02-07 19:39:53 +0200

    more tests & add close server

--- tests/simple-service.test.js
+++ tests/simple-service.test.js
@@ -20,7 +20,9 @@ jest.mock('winston', () => ({
 
 jest.spyOn(redis, 'createClient').mockImplementation(() => ({
   connect: jest.fn(),
+  get: jest.fn().mockImplementation((key, _cb) => _cb('err')),
   on: jest.fn(),
+  del: jest.fn(),
   set: jest.fn()
 }))
 
@@ -32,13 +34,23 @@ const { Expresszz } = require('../service')
 
 describe('Simple service', () => {
   let service
-  beforeAll(async () => {
+  beforeEach(async () => {
     service = new Expresszz('test-service', 5151,  'redis://redis:6379', 'secret', { apiPrefix: '/test' })
     await service.configureApp()
     service.configRoute('get', 'echo', [(req, res) => {
       return res.status(200).send('this is a test')
     }])
+    service.configRoute('post', 'post', [(req, res) => {
+      return res.status(200).send('this is a post test')
+    }])
+    service.configRoute('put', 'put', [(req, res) => {
+      return res.status(200).send('this is a put test')
+    }])
     await service.run()
+  })
+
+  afterEach(async () => {
+    await service.stop()
   })
 
   test('Not found',  (done) => {
@@ -57,6 +69,26 @@ describe('Simple service', () => {
       .expect(201)
       .end((err, res) => {
         expect(res.text).toEqual('this is a test')
+        done()
+      })
+  })
+
+  test('On post',  (done) => {
+    request(service.app)
+      .post('/test/post')
+      .expect(201)
+      .end((err, res) => {
+        expect(res.text).toEqual('this is a post test')
+        done()
+      })
+  })
+
+  test('On put',  (done) => {
+    request(service.app)
+      .put('/test/put')
+      .expect(201)
+      .end((err, res) => {
+        expect(res.text).toEqual('this is a put test')
         done()
       })
   })
